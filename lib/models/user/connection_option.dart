76083ad31b961085c72ae066718cc4df9c496937
cnurba <cnurba@gmail.com>
2020-06-10 15:31:57 +0600

    added button demo for

--- lib/models/user/connection_option.dart
+++ lib/models/user/connection_option.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+
 class ConnectionOption {
   String login;
   String password;
@@ -6,8 +8,26 @@ class ConnectionOption {
   String port;
 
 
+  ConnectionOption({this.login, this.password, this.ip, this.http, this.port});
 
+  factory ConnectionOption.fromJson(){
+    return null;
+  }
 
+//  factory CashModel.FromJon (Map<String,dynamic> parsedJson){
+//
+//    return new CashModel(
+//      organization: parsedJson["organization"],
+//      account: parsedJson["account"],
+//      subconto: parsedJson["subconto"],
+//      amount: parsedJson["amount"],
+//      currencyAmount: parsedJson["currencyAmount"],
+//      comment: parsedJson["comment"],
+//
+//    );
+
+  }
 
 
 }
+
