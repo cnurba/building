dc624e312d4ea364bf546e386148d40fb642fbd5
cnurba <cnurba@gmail.com>
2020-06-10 21:45:07 +0600

    Changed the class AppTheme

--- lib/blocs/connection/connect_state.dart
+++ lib/blocs/connection/connect_state.dart
@@ -7,24 +7,13 @@ abstract class ConnectState extends Equatable {
     return [];
   }
 }
+//waiting to see if the user is authenticated or not on app start
+class ConnectUninitialized  extends ConnectState {}
+//waiting to persist/delete a token
+class ConnectLoading extends ConnectState{}
+//successfully authenticated
+class ConnectAuthenticated  extends ConnectState{}
+//not authenticated
+class ConnectUnauthenticated  extends ConnectState{}
 
-class ConnectLoading extends ConnectState {
-
-}
-
-class DirectorConnected extends ConnectState {
-  final ConnectionOption connectionOption;
-
-  DirectorConnected({this.connectionOption});
-}
-
-
-class ErrorState extends ConnectState{
-  String errorText ;
-  ErrorState({this.errorText});
-
-}
-
-
-class ConnectUninitialized extends ConnectState{}
 
